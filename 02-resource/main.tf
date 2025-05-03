8f3117f6e0bc5c83e91cda4b48a624675d06679a
Anil <anil@test.com>
2025-05-03 18:19:22 +0530

    roboshop terraform

--- 02-resource/main.tf
+++ 02-resource/main.tf
@@ -1,8 +1,8 @@
-provider "null" {}
-
-resource "null_resource" "test" {}
-resource "null_resource" "test1" {}
-
-output "test" {
-  value = null_resource.test.id
-}
+# provider "null" {}
+#
+# resource "null_resource" "test" {}
+# resource "null_resource" "test1" {}
+#
+# output "test" {
+#   value = null_resource.test.id
+# }

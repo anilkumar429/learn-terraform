5fb2281bc37cf129130b2b434171a4f4d1b6532c
Anil <anil@test.com>
2025-05-06 00:21:46 +0530

    roboshop terraform

--- 11-remote-state/main.tf
+++ 11-remote-state/main.tf
@@ -4,9 +4,9 @@ terraform {
   backend "azurerm" {
     use_cli              = true
     #tenant_id            = "00000000-0000-0000-0000-000000000000"
-    subscription_id      = "323379f3-3beb-4865-821e-0fff68e4d4ca"
-    resource_group_name  = "project-setup-1"
-    storage_account_name = "rdevopsb84tfstates"
+    subscription_id      = "7d58fd8a-e8be-4f50-b9f6-03616700d9fc"
+    resource_group_name  = "project-1"
+    storage_account_name = "apps11tfstate"
     container_name       = "roboshop-state-files"
     key                  = "test.terraform.tfstate"
   }

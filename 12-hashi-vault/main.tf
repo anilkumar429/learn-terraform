60842f740816ed3f9a85245215f89e6fe7d07eab
Anil <anil@test.com>
2025-05-07 22:11:59 +0530

    roboshop project

---
+++ 12-hashi-vault/main.tf
@@ -0,0 +1,23 @@
+provider "vault" {
+  address = "http://vault.apps11.shop:8200"
+  token = var.token
+}
+
+variable "token" {}
+
+data "vault_generic_secret" "secret" {
+  path = "demo/ssh"
+}
+
+resource "local_file" "foo" {
+  content  = jsonencode(data.vault_generic_secret.secret.data)
+  filename = "/tmp/vault"
+}
+
+resource "local_file" "foo1" {
+  content  = data.vault_generic_secret.secret.data["password"]
+  filename = "/tmp/vault-pass"
+}
+
+
+#terraform
